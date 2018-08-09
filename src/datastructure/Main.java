dadbb2971f09772bccbffad7e48e3f36141b6c6c
Sujin <sujinki6020@gmail.com>
2018-08-09 13:12:38 +0900

    Sujin : LinkedList / Stack / LinearQueue / CircularQueue 자바로 구현해 보기

---
+++ src/datastructure/Main.java
@@ -0,0 +1,64 @@
+package datastructure;
+
+public class Main{
+	
+	// LinkedList
+//	  public static void main(String[] args) {
+//	        LinkedList numbers = new LinkedList();
+//	        numbers.addLast(10);
+//	        numbers.addLast(20);
+//	        numbers.addLast(30);
+//	        LinkedList.ListIterator li = numbers.listIterator();
+//	        while(li.hasNext()){
+//	            if((int)li.next() == 15)
+//	                li.remove();
+//	        }
+//	        System.out.println(numbers);
+//	  }
+	
+	// Stack
+//	public static void main(String[] args) {
+//		Stack stack = new Stack(5);
+//		stack.Push(5);
+//		stack.Push(4);
+//		stack.Push(3);
+//		stack.Push(2);
+//		stack.Push(1);
+//		System.out.println(stack.Pop());
+//		System.out.println(stack.Pop());
+//		System.out.println(stack.Pop());
+//		System.out.println(stack.Pop());
+//		System.out.println(stack.Pop());
+//	}
+	
+	// LinearQueue
+//	public static void main(String[] args) {
+//		LinearQueue lq = new LinearQueue(5);
+//		lq.enqueue(1);
+//		lq.enqueue(2);
+//		lq.enqueue(3);
+//		lq.enqueue(4);
+//		lq.enqueue(5);
+//		System.out.println("1 : " + lq.rear);
+//		lq.dequeue();
+//		System.out.println("2 : " + lq.rear);
+//		lq.dequeue();
+//		System.out.println("3 : " + lq.rear);
+//	}
+	
+	// CircularQueue
+	public static void main(String[] args) {
+		CircularQueue cq = new CircularQueue(5);
+		cq.enqueue(10);
+		cq.enqueue(20);
+		cq.enqueue(30);
+		cq.enqueue(40);
+		cq.enqueue(50);
+		System.out.println("1 : " + cq.rear + "/" + cq.front);
+		cq.dequeue();
+		System.out.println("2 : " + cq.rear + "/" + cq.front);
+		cq.dequeue();
+		System.out.println("3 : " + cq.rear + "/" + cq.front);
+		
+	}
+}
---
+++ src/datastructure/Stack.java
@@ -0,0 +1,34 @@
+package datastructure;
+
+public class Stack {
+	
+	int maxSize = 0;
+	int top = 0;
+	int[] stack;
+	
+	public Stack(int maxSize) {
+		this.maxSize = maxSize;
+		stack = new int[maxSize];
+	}
+	
+	// push : maxSize보다 작을 경우에만 data 삽입 
+	public void Push(int number) {
+		if(top < maxSize) {
+			stack[top] = number;
+			top++;
+		}
+		else {
+			System.out.println("Overflow");
+		}
+	}
+	
+	// top element 반환
+	public Object Pop() {
+		if(top>0) {
+			return stack[--top];
+		} else {
+			System.out.println("No Data in Stack");
+			return null;
+		}
+	}
+}
