93f46fdfe6faafc7b1ebce0bf1b5c75f45ea9a67
Sujin <sujinki6020@gmail.com>
2018-08-09 22:19:37 +0900

    자료구조 코드 구현 경로 변경

---
+++ src/algostudy/sujin/datastructure/LinearQueue.java
@@ -0,0 +1,34 @@
+package algostudy.sujin.datastructure;
+
+public class LinearQueue {
+	int rear = -1;
+	int front = 0;
+	int maxSize = 0;
+	int[] linearQueue;
+	
+	public LinearQueue(int maxSize) {
+		this.maxSize = maxSize;
+		linearQueue = new int[maxSize];
+	}
+	
+	public void enqueue (int num) {
+		if(rear != maxSize-1) {
+			linearQueue[++rear] = num;
+		} else {
+			System.out.println("Data Full");
+		}
+	}
+	
+	public int dequeue() {
+		if(rear!=front || (rear==0&&front==0)) {
+			int temp = linearQueue[front];
+			for(int i=1; i<rear; i++) {
+				linearQueue[i-1] = linearQueue[i];
+			}
+			rear--;
+			return temp;
+		} else {
+			return -1;
+		}
+	}
+}
