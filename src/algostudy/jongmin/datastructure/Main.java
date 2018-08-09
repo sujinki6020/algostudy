fe514497a1770ae8ec17ebccb009334a884fb983
DESKTOP-U4MQOVV\da270 <da6225@gmail.com>
2018-08-09 12:57:01 +0900

    Stack / Queue

--- src/algostudy/jongmin/datastructure/Main.java
+++ src/algostudy/jongmin/datastructure/Main.java
@@ -3,5 +3,7 @@ package algostudy.jongmin.datastructure;
 public class Main {
 	public static void main(String[] args) {
 		LinkedList numbers = new LinkedList();
+		Stack<Integer> s = new Stack<Integer>();
+		Queue<Integer> q = new Queue<Integer>();
 	}
 }
---
+++ src/algostudy/jongmin/datastructure/Queue.java
@@ -0,0 +1,60 @@
+package algostudy.jongmin.datastructure;
+
+import java.util.NoSuchElementException;
+
+public class Queue<T> {
+	class Node<T>{
+		private T data;
+		private Node<T> next;
+		
+		public Node(T data) {
+			this.data = data;
+		}
+	}
+	private Node<T> first;
+	private Node<T> last;
+	
+	public void add(T item) {
+		Node<T> t = new Node<T>(item);
+		
+		if(last != null) {
+			last.next = t;
+		}
+		last = t;
+		if(first == null) {
+			first = last;
+		}
+	}
+	
+	public T remove() {
+		if(first == null) {
+			throw new NoSuchElementException();
+		}
+		
+		T data = first.data;
+		first = first.next;
+		
+		if(first == null) {
+			last = null;
+		}
+		return data;
+	}
+	public T peek() {
+		if(first == null) {
+			throw new NoSuchElementException();
+		}
+		return first.data;
+	}
+	public boolean isEmpty() {
+		return first == null;
+	}
+}
+
+
+
+
+
+
+
+
+
