7edc013cd3f43b0151f6000d8aec8446e821c36b
Sujin <sujinki6020@gmail.com>
2018-08-18 22:24:00 +0900

    백준 단계별로 풀어보기 #2839 설탕 배달

--- src/algostudy/sujin/algorithm/BOJ2839.java
+++ src/algostudy/sujin/algorithm/BOJ2839.java
@@ -1,7 +1,42 @@
 package algostudy.sujin.algorithm;
 
+import java.util.Scanner;
+
 public class BOJ2839 {
+	static int n = 0;
+	static int cnt = 0;
+	
 	public static void main(String[] args) {
-		System.out.println("eclipse 재설정 test");
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		recur(n);
+		System.out.println(cnt);
 	}
+	
+	private static void recur(int n) {
+		if (n>0) {
+			if(n%5==0) {
+				cnt = n/5;
+			} else if(n%3==0) {
+				n -= 3;
+				cnt++;
+				if(n%5==0) {
+					cnt += n/5;
+				} else {
+					recur(n);
+				}
+			} else {
+				n -= 3;
+				cnt++;
+				if(n%5==0) {
+					cnt += n/5;
+				} else {
+					recur(n);
+				}
+			}
+		} else {
+			cnt = -1;
+		}
+	}
+
 }
