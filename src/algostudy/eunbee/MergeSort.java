2d1c67dace5931d99de5fc00dd43b593dffb7731
이은비 <lob1224@hanmail.net>
2018-08-18 16:53:51 +0900

    mergeSort, QuickSort, 즐거운 파티 구현

---
+++ src/algostudy/eunbee/MergeSort.java
@@ -0,0 +1,46 @@
+package algostudy.eunbee;
+/*
+ * 배열정렬
+ * */
+public class MergeSort {
+
+	private static void mergeSort(int[] arr) {
+		int[] tmp = new int[arr.length];
+		mergeSort(arr, tmp, 0, arr.length -1);
+	}
+	private static void mergeSort(int[] arr, int[] tmp, int start, int end) {
+		if(start < end) {
+			int mid = (start + end) / 2;
+			mergeSort(arr, tmp, start, mid);
+			mergeSort(arr, tmp, mid + 1, end);
+			merge(arr, tmp, start, mid, end);
+		}
+	}
+	private static void merge(int[] arr, int[] tmp, int start, int mid, int end) {
+		for(int i = start; i<= end; i++) {
+			tmp[i] = arr[i];
+		}
+		int part1 = start;
+		int part2 = mid +1;
+		int index = start;
+		while(part1 <= mid && part2 <= end) {
+			if(tmp[part1] <= tmp[part2]) {
+				arr[index] = tmp[part1];
+				part1++;
+			}else {
+				arr[index] = tmp[part2];
+				part2++;
+			}
+			index++;
+		}
+		for(int i = 0; i<= mid - part1; i++) {
+			arr[index + i] = tmp[part1 + i];
+		}
+	}
+	private static void printArray(int[] arr) {
+		for(int data : arr) {
+			System.out.print(data + ", ");
+		}
+		System.out.println();
+	}
+}
