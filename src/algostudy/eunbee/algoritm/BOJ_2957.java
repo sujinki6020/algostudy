5a6911111bd4deb214f26522cb5dd30e4148b712
이은비 <lob1224@hanmail.net>
2018-11-15 01:04:41 +0900

    백준 이진탐색트리_2957

--- src/algostudy/eunbee/algoritm/BOJ_2957.java
+++ src/algostudy/eunbee/algoritm/BOJ_2957.java
@@ -22,37 +22,67 @@ public class BOJ_2957 {
 	
 	public static void getDepth(List<Integer> list) {
 		
+		//최대, 최소값 저장할 곳
 		List<Integer> maxList = new ArrayList<>();
+		List<Integer> minList = new ArrayList<>();
+		//
 		List<Integer> nodeList = new ArrayList<>();
-		
-		int fullIndex = 1;
+		//노드들이 차고있는 현황 배열
+		List<Integer> fullArr = new ArrayList<>(list.size());
+		//0으로 초기화
+		for(int i :fullArr) {
+			fullArr.set(i, 0);
+		}
 		int min = 0;
 		int max = 0; 
 		
 		for(int i = 0; i < list.size(); i++) {
 			int num = list.get(i);
+			//0일때
 			if(max == 0 || min == 0 ) {
-				max = num; 
-				min = num;
-				nodeList.add(num);
-			}
-//			if()
-//			if(max < num) {
-//				max = num;
-//				maxList.add(max);
-//			}
-//			
-//			
-//			if(num < max && num < min) {
-//				count++;
-//			}
-//			
-//			
-//			if(fullIndex == count) {
-//				fullIndex++;
-//				count = 0;
-//			}
+				max = num; min = num;
 				
+				maxList.add(max);
+				minList.add(min);
+				fullArr.set(i, fullArr.get(i)+1);
+			}
+			
+			if(num > min && num < max) {
+				//꽉찬 노드 제외하고 맥스 아래부터 탐색
+				int size = maxList.size()>minList.size()? maxList.size(): minList.size();
+				for(int j =0; j < size; j++) {
+					//범위안에 들어있으면 깊이가 꽉차있는지 확인한다
+					if(num <maxList.get(j) && num<minList.get(i)) {
+						if(fullArr.get(maxList.indexOf(maxList.get(j))) == fullArr.indexOf(maxList.indexOf(maxList.get(j)))) {
+							
+						}
+					}
+				}
+				
+				
+			}else {
+				if(num > max) {
+					max = num;
+					maxList.add(max);
+					
+					int ioMax = maxList.indexOf(max);
+					System.out.println(ioMax);
+					
+					//fullArr채우기
+					fullArr.set(ioMax-1, fullArr.get(ioMax-1)+1);
+				}
+				if(num < min) {
+					min = num;
+					minList.add(min);
+					int ioMin = minList.indexOf(min);
+					System.out.println(ioMin);
+					
+					//fullArr채우기
+					fullArr.set(ioMin-1, fullArr.get(ioMin-1)+1);
+					
+				}
+			}
+			
 		}
 		
 	}
