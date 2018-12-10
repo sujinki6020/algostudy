eea7282453facf7bfc539542d3808dbfb64b6203
eunbee <eunbee1224@gmail.com>
2018-12-10 19:01:39 +0900

    백준_3412

---
+++ src/algostudy/eunbee/algoritm/BOJ_3412.java
@@ -0,0 +1,51 @@
+package algostudy.eunbee.algoritm;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class BOJ_3412 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int test = sc.nextInt();
+		List<List> testList = new ArrayList<>();
+		for(int i = 0; i< test; i++) {
+			int testCase = sc.nextInt();
+			List<String> testCaseList = new ArrayList<>();
+			for(int j = 0; j < testCase; j++) {
+				testCaseList.add(sc.nextLine());
+			}
+			testList.add(testCaseList);
+		}
+		
+		for(int i = 0; i < testList.size(); i++) {
+			getCases(testList.get(i));
+		}
+		
+		
+	}
+	
+	public static void getCases(List<String> list) {
+		int score = 0;
+		for(int i = 0; i <list.size(); i++) {
+			int x = Integer.parseInt(list.get(i).split(" ")[0]);
+			int y = Integer.parseInt(list.get(i).split(" ")[1]);
+			score += lengthCount(x,y);
+		}
+		
+	}
+	
+	public static int lengthCount(int x, int y) {
+		double length = Math.sqrt((x*x)+(y*y));
+		int score = 0;
+		for(int i = 0 ; i < 10; i *=20) {
+			if( i > length) {
+				score = i;
+				break;
+			}
+			
+		}
+		return score;
+	}
+}
