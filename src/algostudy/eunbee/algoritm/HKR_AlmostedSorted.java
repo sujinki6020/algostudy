7f8f57c5c2bf0df83fb5819386a7af87cbf26948
이은비 <lob1224@hanmail.net>
2019-02-12 02:00:04 +0900

    HKR_almost sorted

--- src/algostudy/eunbee/algoritm/HKR_AlmostedSorted.java
+++ src/algostudy/eunbee/algoritm/HKR_AlmostedSorted.java
@@ -8,6 +8,8 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
+import com.sun.xml.internal.bind.util.Which;
+
 public class HKR_AlmostedSorted {
 
     // Complete the almostSorted function below.
@@ -17,57 +19,74 @@ public class HKR_AlmostedSorted {
     	
     	int prePreNum =arr[0];
     	int preNum =arr[1];
-    	int startNum = prePreNum>preNum? prePreNum: 0;
-    	int endNum = 0;
+    	int startIndex = prePreNum>preNum? 1: 0;
     	
-    	if(arr.length == 2 && arr[0] < arr[1]) {
+    	if(arr.length == 2 && arr[0] > arr[1]) {
     		result = "yes";
-    		cases = "swap"+arr[0] + arr[1];
+    		cases = "swap 1 2";
     	}
         for(int i = 2; i<arr.length; i++) {
-        	if(prePreNum > preNum && preNum < arr[i]) {
-        		cases = "swap";
+        	if(cases == null) {
+	        	if(prePreNum > preNum && preNum < arr[i]) {
+	        		cases = "swap";
+	        		i = i-1;
+	        	}
+	        	if(prePreNum > preNum && preNum > arr[i]) {
+	        		cases = "reverse";
+	        		i = i-1;
+	        	}
+	        	startIndex = i-2;
+	        	prePreNum = preNum;
         	}
-        	if(prePreNum > preNum && preNum < arr[i]) {
-        		cases = "swap";
-        	}
-        	
-        	if(startNum ==0 && preNum > arr[i] ) {
-        		startNum = preNum;
-        	}else if(startNum != 0) {
-        		if(cases == null && preNum > arr[i]) {
-        			cases = "reverse";
-        			
-        		}
-        		if(cases.equals("swap") && (preNum > arr[i] || i == arr.length-1)) {
-        			endNum = arr[i];
+        	if(cases != null) {
+        		
+        	switch(cases){
+        		case"swap":
+        			if(preNum > arr[i]) {
+//        				if( arr[i] >arr[startIndex-1] && arr[i]< arr[startIndex+1] && arr[i-1] < arr[startIndex]) {
+        				if( arr[i] >arr[startIndex-1] && arr[i]<= arr[startIndex+1] && arr[i-1] < arr[startIndex]) {
+        					result ="yes";
+        					cases += " "+(startIndex+1)+" "+(i+1);
+        				}
+        			}
+        			break;
+        		case "reverse":
+        			if(preNum < arr[i]) {
+        				if( preNum >arr[startIndex-1] && preNum < arr[startIndex+1] && arr[startIndex] < arr[i]) {
+        					result ="yes";
+        					cases += " "+(startIndex+1)+" "+i;
+        				}
+        			}
+        			break;
         		}
         	}
         	preNum = arr[i];
         }
         System.out.println(result);
         if(result.equals("yes")) System.out.println(cases);
-
     }
 
-    private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int n = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        int[] arr = new int[n];
-
-        String[] arrItems = scanner.nextLine().split(" ");
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        for (int i = 0; i < n; i++) {
-            int arrItem = Integer.parseInt(arrItems[i]);
-            arr[i] = arrItem;
-        }
-
+//    	Scanner scanner = new Scanner(System.in);
+//        int n = scanner.nextInt();
+////        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+//
+//        int[] arr = new int[n];
+//
+//        String arrItem = scanner.nextLine();
+//        String[] arrItems = arrItem.split(" ");
+//        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+//
+//        for (int i = 0; i < n; i++) {
+//            arr[i] = Integer.parseInt(arrItems[i]);
+//        }
+    	int[] arr = 
+    		{
+    				1 ,2, 3, 5, 4, 6
+    		};
         almostSorted(arr);
 
-        scanner.close();
+//        scanner.close();
     }
 }
